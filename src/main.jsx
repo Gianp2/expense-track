7a136d5cad3095d2a8d37c0739234ff2756b5d1a
Gianluca Pasquinelli <117898176+Gianp2@users.noreply.github.com>
2025-07-17 19:44:02 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  // <-- cambiar aquí
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/expense-track"> {/* 👈 RUTA BASE agregada */}
+    <HashRouter basename="/expense-track"> {/* 👈 reemplazado BrowserRouter por HashRouter */}
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
