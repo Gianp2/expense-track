1d81bdd990a1f941d3db3722a5f66c47a1368e84
Gianluca Pasquinelli <117898176+Gianp2@users.noreply.github.com>
2025-07-17 19:57:34 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";  // <-- cambiar aquí
+import { HashRouter } from "react-router-dom"; 
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/expense-track"> {/* 👈 reemplazado BrowserRouter por HashRouter */}
+    <HashRouter> 
       <AuthProvider>
         <App />
       </AuthProvider>
